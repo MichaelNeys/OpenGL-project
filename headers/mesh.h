63449c6eb2608a7ab6094293f0aa03c3e265fbb3
MaximilliaanJA-2467787 <maximiliaan.janssen@student.uhasselt.be>
2026-04-23 20:48:54 +0200

    Toevoegen atomium

--- headers/mesh.h
+++ headers/mesh.h
@@ -1,21 +1,43 @@
-#ifndef MESH_H
-#define MESH_H
-
+#pragma once
 #include <glad/glad.h>
+#include <glm/glm.hpp>
 #include <vector>
+#include <string>
+#include "shader.h"
 
 class Mesh {
 public:
-    unsigned int VAO;
+    unsigned int VAO, VBO;
+    unsigned int vertexCount, stride;
 
+    // Bestaande constructor (voor kubus/lamp)
     Mesh(float* vertices, unsigned int vertexCount, bool hasNormals);
     void Draw();
     void Delete();
 
-private:
-    unsigned int VBO;
-    unsigned int vertexCount;
-    int stride;
-};
+    // Assimp modellen
+    struct Vertex {
+        glm::vec3 Position;
+        glm::vec3 Normal;
+        glm::vec2 TexCoords;
+    };
+    struct Texture {
+        unsigned int id;
+        std::string  type;  
+        // "texture_diffuse" of "texture_normal"
+        std::string  path;
+    };
 
-#endif
+    // Nieuwe constructor
+    Mesh(std::vector<Vertex> vertices, std::vector<unsigned int> indices, std::vector<Texture> textures);
+    void Draw(Shader& shader);  // overload voor model-rendering
+    void DeleteModel();
+
+private:
+    // alleen gebruikt door model-constructor
+    unsigned int EBO;
+    std::vector<Vertex>       _vertices;
+    std::vector<unsigned int> _indices;
+    std::vector<Texture>      _textures;
+    void setupModelMesh();
+};
